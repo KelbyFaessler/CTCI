7ab4354264139e344909f019dc153dc0780eefe1
Kelby Faessler <kmfaessler@gmail.com>
2018-01-14 10:36:21 -0800

    new project for testing various features of C++ language

---
+++ Experiments/hashmaps/main.cpp
@@ -0,0 +1,17 @@
+/*
+Program to play with sets and maps, both ordered and unordered.
+*/
+
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+int main(int argc, char* argv[])
+{
+    
+
+
+
+    return 0;
+}
