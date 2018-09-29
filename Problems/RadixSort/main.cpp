c3b0e526be1b19c86f14267b075357ac2bf91949
Kelby Faessler <kmfaessler@gmail.com>
2018-09-29 08:12:30 -0700

    added project for radix sort

---
+++ Problems/RadixSort/main.cpp
@@ -0,0 +1,27 @@
+/*
+Project to implement radix sort from scratch.
+
+Radix sort properties:
+  -Only works on certain data types (e.g integers, strings)
+
+Radix sort algorithm:
+  -Sort all integers by least significant digit
+  -Sort all integers by 2nd least significant digit
+    ...
+  -Sort all integers by most significant digit
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main(int argc, char* argv[])
+{
+    //Don't actually need anything in main here right now
+
+    //keep console open
+    std::cin.get();
+
+    return 0;
+}
