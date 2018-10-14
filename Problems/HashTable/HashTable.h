d549eaa68f61962b160620af05707323c8cd5e24
Kelby Faessler <kmfaessler@gmail.com>
2018-10-14 12:08:50 -0700

    started on hash table project

---
+++ Problems/HashTable/HashTable.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+* Implementation of a hash table from scratch for practice
+*
+* Assumptions:
+*    The keys are strings
+*    The values are ints
+*    I'm not going to try and generalize with templates
+******************************************************************************/
+#pragma once
+
+class HashTable
+{
+    public:
+
+
+    private:
+        int size;
+        int capacity;
+
+};
---
+++ Problems/HashTable/main.cpp
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * Program entry point for testing my implementation of a hash table
+******************************************************************************/
+
+#include "HashTable.h"
+
+int main(int argc, const char* argv[])
+{
+
+
+    return 0;
+}
