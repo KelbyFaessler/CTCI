b01c0e6c668ea8a687f4417df3bedf19bf4cb471
Kelby Faessler <kmfaessler@gmail.com>
2018-10-14 17:40:45 -0700

    figuring out how to structure hash table class

--- Problems/HashTable/HashTable.h
+++ Problems/HashTable/HashTable.h
@@ -5,16 +5,24 @@
 *    The keys are strings
 *    The values are ints
 *    I'm not going to try and generalize with templates
+*    Resize the hash table whenever the number of elements equals the size of array
 ******************************************************************************/
 #pragma once
+
+#include <string>
+//#include <vector>
 
 class HashTable
 {
     public:
-
+        void Insert(std::string& key, int value);
+        int& operator[](std::string& key);
+        const int operator[](std::string& key);
 
     private:
         int size;
         int capacity;
+        //int data[]; do I want a C-style array or a vector?
 
+        void Resize();
 };
