77be020c326bd9eac7f1b6b51627acd96c83d2aa
Kelby Faessler <kmfaessler@gmail.com>
2018-10-20 11:52:26 -0700

    added quicksort template to be used as starting point for practice implementations

---
+++ Problems/QuickSort/QuicksortTemplate.cpp
@@ -0,0 +1,12 @@
+/******************************************************************************
+* Description: Provides an empty skeleton as a starting point for quicksort
+* implemenation practice.
+*
+* Note: change the class name if using the template
+*
+* Comment for implementation: (delete this line + everything above, keep below)
+* Description: Implementation of the quicksort algorithm; practice attempt #1.
+******************************************************************************/
+#include "Quicksort1.h"
+#include "Utils.h"
+
---
+++ Problems/QuickSort/QuicksortTemplate.h
@@ -0,0 +1,24 @@
+/******************************************************************************
+* Description: Provides an empty skeleton as a starting point for quicksort
+* implemenation practice. 
+*
+* Note: change the class name if using the template
+*
+* Comment for implementation: (delete this line + everything above, keep below)
+* Description: Implementation of the quicksort algorithm; practice attempt #1.
+******************************************************************************/
+#pragma once
+
+#include "QuicksortBase.h"
+#include <vector>
+
+class QuicksortX : public QuicksortBase
+{
+public:
+    //first create own function declarations here and definitions in cpp. Only
+    //then look at QuicksortBase interface and adjust if necessary.
+
+private:
+    //first create own function declarations here and definitions in cpp. Only
+    //then look at QuicksortBase interface and adjust if necessary.
+};
